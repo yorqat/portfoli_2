bfd81a79ef7a0eb5f50b020c0a1c4d7413c48f44
YorQat <qarkdev+gh@gmail.com>
2026-02-10 13:24:03 +0800

    update vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,9 @@
 import { sveltekit } from '@sveltejs/kit/vite'
 import { enhancedImages } from '@sveltejs/enhanced-img'
 import { defineConfig } from 'vite'
-import kit10 from '../kit10/plugin/vite-kit10-plugin.js'
 
 export default defineConfig({
 	plugins: [
-    kit10(),
 		enhancedImages(), // must come first
 		sveltekit()
 	]
